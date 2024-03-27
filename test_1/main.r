04edd7dca1f908ce5a600ef96fe7c1923f74b0b5
ARna06 <72038543+Leopard005537@users.noreply.github.com>
2024-03-27 13:05:59 +0530

    Update

--- test_1/main.r
+++ test_1/main.r
@@ -1,13 +1,13 @@
-library("volesti")
-library("rgl")
-polytope <- gen_cube(100, "H")
+library('volesti')
+library('rgl')
 
-samples <- sample_points(polytope, n = 1000, random_walk = list(
-    "walk" = "CDHR", "burn-in" = 1000, "walk_length" = 5
-))
-#here we use coordinate direction hit and run random walk scheme,
-#to sample the points from the hypercube.
-
-#we generate a plot of the sampled points using only the coordinates.
-plot3d(samples, xlab = "x", ylab = "y", zlab = "z") 
+walk = "RDHR"
+b = c(1,1,1,1,1)
+A = matrix(c(-1,0,0,0,-1,0,0,0,-1,1,0,0,0,1,0,0,0,1), nrow = 6, ncol = 3, byrow = T)
+P = Hpolytope(A=A, b=b)
+points = sample_points(P, 100)
+plot3d(x = points[1,], y = points[2,], z = points[3,],
+       xlab = "X", ylab = "Y", zlab = "Z", 
+       type = 'p',
+       title3d(main = sprintf("Sampling a random cube with walk %s", walk)))
 rglwidget()
